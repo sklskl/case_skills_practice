1651218aa6722ba3d1cbb2a2a27a043720cd027a
sunkeling <454541398@qq.com>
2023-06-18 19:21:34 +0800

    add .

--- src/com/skl/arraytest/ArrayTest07.java
+++ src/com/skl/arraytest/ArrayTest07.java
@@ -4,5 +4,6 @@ public class ArrayTest07 {
     public static void main(String[] args) {
         System.out.println('a');
         System.out.println('a');
+        System.out.println('a');
     }
 }
