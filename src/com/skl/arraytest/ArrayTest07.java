1793ea1280e1361b8993b30139dc7f0a6be53cdb
sunkeling <454541398@qq.com>
2023-06-18 19:36:35 +0800

    skl

--- src/com/skl/arraytest/ArrayTest07.java
+++ src/com/skl/arraytest/ArrayTest07.java
@@ -4,6 +4,5 @@ public class ArrayTest07 {
     public static void main(String[] args) {
         System.out.println('a');
         System.out.println('a');
-        System.out.println('a');
     }
 }
