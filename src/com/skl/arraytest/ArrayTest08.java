6ef18b02eab1d65e4fbd4187c85a92d816ffb9b8
sunkeling <454541398@qq.com>
2023-06-27 23:38:36 +0800

    add .

--- src/com/skl/arraytest/ArrayTest08.java
+++ src/com/skl/arraytest/ArrayTest08.java
@@ -1,4 +1,12 @@
 package com.skl.arraytest;
 
+import java.util.Arrays;
+
 public class ArrayTest08 {
+    public static void main(String[] args) {
+        int [] srcarr = {1,4,6,8,9,10,39,59};
+        int [] destarr = new int[10];
+        System.arraycopy(srcarr, 1,destarr,3,3);
+        System.out.println(Arrays.toString(destarr));
+    }
 }
--- src/com/skl/arraytest/ArrayTest09.java
+++ src/com/skl/arraytest/ArrayTest09.java
@@ -1,4 +1,13 @@
 package com.skl.arraytest;
 
+import java.util.ArrayList;
+
 public class ArrayTest09 {
+    public static void main(String[] args) {
+        System.out.println('s');
+        new ArrayList<Integer>();
+    }
+    public  void eat(){
+        return;
+    }
 }
